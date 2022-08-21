47ee5eb6377cc112f4460983ec24b68dcb39ad82
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-21 23:14:33 +0900

    chore: Fix jest setting

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({
-	dir: './'
-});
-
-const customJestConfig = {
-	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-	moduleDirectories: ['node_modules', '<rootDir>/'],
-	testEnvironment: 'jest-environment-jsdom'
+module.exports = {
+	setupFilesAfterEnv: ['./jest.setup.js'],
+	moduleNameMapper: {
+		'^@components(.*)$': '<rootDir>/components$1',
+		'^@pages(.*)$': '<rootDir>/pages$1',
+		'^@hooks(.*)$': '<rootDir>/hooks$1'
+	}
 };
-
-module.exports = createJestConfig(customJestConfig);

670af7004b943994548f2e10edc46219cba79e31
장동현 <54137044+ww8007@users.noreply.github.com>
2022-08-28 00:00:12 +0900

    feat: Add Head with meta, title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import GlobalStyle from '../GlobalStyle';
 import * as React from 'react';
 import { app } from '../firebaseconfig';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
 	React.useEffect(() => {
 		return () => {
@@ -11,6 +13,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 	}, []);
 	return (
 		<>
+			<Head>
+				<title>JJ 탁구장 주차장</title>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+				/>
+			</Head>
 			<GlobalStyle />
 			<RecoilRoot>
 				<Component {...pageProps} />
